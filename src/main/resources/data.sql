32cafd9da0a797ab0c88f296cfebf464e04847f1
Anish Maharjan <amaharjan@miu.edu>
2021-12-10 12:34:46 -0600

    added data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,3 @@
-INSERT INTO COURSE (id, code, name) VALUES (1, '545', 'WAA');
-INSERT INTO COURSE (id, code, name) VALUES (2, '472', 'WAP');
-INSERT INTO COURSE (id, code, name) VALUES (3, '521', 'EA');
-INSERT INTO COURSE (id, code, name) VALUES (4, '566', 'ASD');
-INSERT INTO COURSE (id, code, name) VALUES (5, '453', 'ALG');
-
-create sequence if not exists course start with 6;
+INSERT INTO ROLE (id, role) VALUES (1, 'ADMIN');
+INSERT INTO ROLE (id, role) VALUES (2, 'SELLER');
+INSERT INTO ROLE (id, role) VALUES (3, 'BUYER');
